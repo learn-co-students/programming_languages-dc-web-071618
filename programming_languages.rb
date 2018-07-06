4511e4071c06fb3209471601a9f0517d9a15fafa
Jon Schulman <schulman99@gmail.com>
2018-07-06 18:49:03 +0000

    Done.

--- programming_languages.rb
+++ programming_languages.rb
@@ -19,9 +19,9 @@ def reformat_languages(languages)
 
   languages.each do |oo_f, language|
     language.each do |name, info|
-      new_hash[name] ||= info
-      new_hash[name][:style] ||= []
-      new_hash[name][:style] << oo_f
+      new_hash[name] ||= info         #adds "info" - which is already in there as :type => "compiled"  (etc.)
+      new_hash[name][:style] ||= []   #creates an empty array as the value of :style (if there's nothing there already)
+      new_hash[name][:style] << oo_f  #pushes oo_f into the :style array - whcich takes care of getting both 'oo' and 'functional' into :javascript
     end
   end
   new_hash
